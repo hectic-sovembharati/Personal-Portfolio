449eac40ce0ea2ba8d850d6289699c89044a1f38
Sovem Jung Bharati <88200119+hectic-sovembharati@users.noreply.github.com>
2021-10-02 12:29:37 +0545

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function myFunction() {
+    var x = document.getElementById("centered_nav");
+    if (x.className === "rc_nav") {
+        x.className += " responsive";
+    } else {
+        x.className = "rc_nav";
+    }
+}
+
+
